8c18695408cd876fab8cacd87605e6027e2dd974
Guo Zhuoning <847738428@qq.com>
2020-03-12 17:12:38 +0800

    Passed Edge/Vertices Graph Test

--- test/P1/graph/GraphInstanceTest.java
+++ test/P1/graph/GraphInstanceTest.java
@@ -57,6 +57,11 @@ public abstract class GraphInstanceTest {
 	assertEquals(true, emptyInstance.add("2"));
 	assertEquals(true, emptyInstance.add("3"));
 	assertEquals(true, emptyInstance.add("4"));
+	assertEquals(false, emptyInstance.add("2"));
+	assertEquals(true, emptyInstance.add("5"));
+	assertEquals(false, emptyInstance.add("1"));
+	assertEquals(true, emptyInstance.add("abcdefghijklmn"));
+	assertEquals(false, emptyInstance.add("abcdefghijklmn"));
     }
 
     @Test
@@ -65,62 +70,145 @@ public abstract class GraphInstanceTest {
 
 	// add
 	assertEquals(true, emptyInstance.add("1"));
-	assertEquals(false, emptyInstance.add("1"));
 	assertEquals(true, emptyInstance.add("2"));
-	assertEquals(true, emptyInstance.add("3"));
-	assertEquals(true, emptyInstance.add("4"));
 
 	// remove
-	assertEquals(true, emptyInstance.remove("4"));
+	assertEquals(true, emptyInstance.remove("2"));
+	assertEquals(false, emptyInstance.remove("2"));
+	assertEquals(false, emptyInstance.remove("2"));
+
+	assertEquals(false, emptyInstance.remove("0"));
+	assertEquals(true, emptyInstance.add("0"));
+	assertEquals(true, emptyInstance.remove("0"));
 	assertEquals(false, emptyInstance.remove("0"));
     }
 
-    @Test(expected = RuntimeException.class)
-    public void testSet1() {
+    @Test
+    public void testSet() {
 	Graph<String> emptyInstance = emptyInstance();
-
-	// add
 	assertEquals(true, emptyInstance.add("1"));
-	assertEquals(false, emptyInstance.add("1"));
 	assertEquals(true, emptyInstance.add("2"));
 	assertEquals(true, emptyInstance.add("3"));
-	assertEquals(true, emptyInstance.add("4"));
-
-	// remove
-	assertEquals(true, emptyInstance.remove("4"));
-	assertEquals(false, emptyInstance.remove("0"));
 
 	// set
 	assertEquals(0, emptyInstance.set("1", "2", 1));
 	assertEquals(1, emptyInstance.set("1", "2", 2));
 	assertEquals(2, emptyInstance.set("1", "2", 1));
+
+	assertEquals(0, emptyInstance.set("3", "3", 1)); // same vertex
+	assertEquals(0, emptyInstance.set("3", "3", 1));
+	assertEquals(0, emptyInstance.set("3", "3", 1)); // MUST be forever 0
+
+	assertEquals(0, emptyInstance.set("1", "3", 4));
+	assertEquals(4, emptyInstance.set("1", "3", 0));
+	assertEquals(0, emptyInstance.set("1", "3", 4));
+	assertEquals(4, emptyInstance.set("1", "3", 0));
+
+	// sources & targets
+	assertEquals(Collections.EMPTY_MAP, emptyInstance.sources("3"));
+	assertEquals(Collections.EMPTY_MAP, emptyInstance.targets("3"));
+
+    }
+
+    @Rule
+    public ExpectedException thrownNegW = ExpectedException.none();
+
+    @Test
+    public void testSetNegW() {
+	Graph<String> emptyInstance = emptyInstance();
+	assertEquals(true, emptyInstance.add("1"));
+	assertEquals(true, emptyInstance.add("2"));
+	assertEquals(true, emptyInstance.add("3"));
+	thrownNegW.expect(RuntimeException.class);
+	thrownNegW.expectMessage("Negative weight");
 	emptyInstance.set("1", "3", -1);
     }
 
     @Rule
-    public ExpectedException thrown = ExpectedException.none();
+    public ExpectedException thrownInexistV = ExpectedException.none();
 
     @Test
-    public void testSet2() {
+    public void testSetInexistV() {
 	Graph<String> emptyInstance = emptyInstance();
 	assertEquals(true, emptyInstance.add("1"));
 	assertEquals(true, emptyInstance.add("2"));
 	assertEquals(true, emptyInstance.add("3"));
-	assertEquals(true, emptyInstance.add("4"));
-	assertEquals(true, emptyInstance.remove("4"));
-
-	// set
-	assertEquals(0, emptyInstance.set("1", "2", 1));
-	assertEquals(1, emptyInstance.set("1", "2", 2));
-	assertEquals(2, emptyInstance.set("1", "2", 1));
-	
-	thrown.expect(RuntimeException.class);
-	thrown.expectMessage("Negative weight");
-	emptyInstance.set("1", "3", -1);
+	thrownInexistV.expect(RuntimeException.class);
+	thrownInexistV.expectMessage("Inexistent vertex");
+	emptyInstance.set("0", "3", 2);
     }
 
     @Test
-    public void test1() {
+    public void testVertices() {
+	Graph<String> emptyInstance = emptyInstance();
+	assertEquals(true, emptyInstance.add("1"));
+	assertEquals(false, emptyInstance.add("1"));
+	assertEquals(true, emptyInstance.add("2"));
+	assertEquals(true, emptyInstance.add("3"));
+	assertEquals(true, emptyInstance.add("4"));
+	assertEquals(false, emptyInstance.add("4"));
+	assertEquals(true, emptyInstance.add("5"));
+	assertEquals(false, emptyInstance.remove("0"));
+	assertEquals(true, emptyInstance.remove("5"));
+	assertEquals(true, emptyInstance.add("6"));
+	assertEquals(true, emptyInstance.remove("6"));
+	assertEquals(true, emptyInstance.add("6"));
+
+	// vertices
+	Set<String> vertices = new HashSet<String>();
+	vertices.add("1");
+	vertices.add("2");
+	vertices.add("3");
+	vertices.add("4");
+	vertices.add("6");
+	assertEquals(vertices, emptyInstance.vertices());
+    }
+
+    @Test
+    public void testSourceTarget() {
+	Graph<String> emptyInstance = emptyInstance();
+	// add
+	assertEquals(true, emptyInstance.add("1"));
+	assertEquals(true, emptyInstance.add("2"));
+	assertEquals(true, emptyInstance.add("3"));
+	assertEquals(true, emptyInstance.add("4"));
+
+	// set
+	assertEquals(0, emptyInstance.set("1", "2", 1));
+	assertEquals(0, emptyInstance.set("1", "3", 3));
+	assertEquals(0, emptyInstance.set("1", "4", 5));
+	assertEquals(0, emptyInstance.set("2", "2", 1));
+	assertEquals(0, emptyInstance.set("2", "2", 1));
+	assertEquals(0, emptyInstance.set("2", "3", 2));
+	assertEquals(0, emptyInstance.set("4", "1", 4));
+
+	// source
+	Map<String, Integer> sources = new HashMap<String, Integer>();
+	sources.put("4", 4); // 4
+	assertEquals(sources, emptyInstance.sources("1"));
+	sources.remove("4"); // empty
+	sources.put("1", 3); // 3
+	sources.put("2", 2); // 2 3
+	assertEquals(sources, emptyInstance.sources("3"));
+	sources.clear(); // empty
+	sources.put("1", 1); // 1
+	assertEquals(sources, emptyInstance.sources("2"));
+	assertEquals(Collections.EMPTY_MAP, emptyInstance.sources("5")); // sources of non-in-vertex should be empty
+
+	// targets
+	Map<String, Integer> targets = new HashMap<String, Integer>();
+	targets.put("2", 1);
+	targets.put("3", 3);
+	targets.put("4", 5); // 2 3 4
+	assertEquals(targets, emptyInstance.targets("1"));
+	assertEquals(Collections.EMPTY_MAP, emptyInstance.targets("3"));
+	targets.clear();
+	targets.put("1", 4);
+	assertEquals(targets, emptyInstance.targets("4"));
+    }
+
+    @Test
+    public void testWhole() {
 	Graph<String> emptyInstance = emptyInstance();
 
 	// add
@@ -138,22 +226,20 @@ public abstract class GraphInstanceTest {
 	assertEquals(0, emptyInstance.set("1", "2", 1));
 	assertEquals(1, emptyInstance.set("1", "2", 2));
 	assertEquals(2, emptyInstance.set("1", "2", 1));
-	// assertEquals(0, emptyInstance.set("1", "3", -1));
-	// assertEquals(0, emptyInstance.set("2", "3", 0));
 
 	// source
 	Map<String, Integer> sources = new HashMap<String, Integer>();
 	sources.put("1", 1);
 	assertEquals(sources, emptyInstance.sources("2"));
-	assertEquals(Collections.emptySet(), emptyInstance.sources("3"));
-	assertEquals(Collections.emptySet(), emptyInstance.sources("4"));
+	assertEquals(Collections.EMPTY_MAP, emptyInstance.sources("3"));
+	assertEquals(Collections.EMPTY_MAP, emptyInstance.sources("4"));
 
 	// targets
 	Map<String, Integer> targets = new HashMap<String, Integer>();
 	targets.put("2", 1);
 	assertEquals(targets, emptyInstance.targets("1"));
-	assertEquals(Collections.emptySet(), emptyInstance.targets("3"));
-	assertEquals(Collections.emptySet(), emptyInstance.targets("5"));
+	assertEquals(Collections.EMPTY_MAP, emptyInstance.targets("3"));
+	assertEquals(Collections.EMPTY_MAP, emptyInstance.targets("5"));
 
 	// vertices
 	Set<String> vertices = new HashSet<String>();
