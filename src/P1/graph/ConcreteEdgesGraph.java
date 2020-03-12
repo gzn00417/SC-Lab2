c9d1d1a9480ec8482e9e4900442f7f7eb62cc370
Guo Zhuoning <847738428@qq.com>
2020-03-12 17:12:32 +0800

    Update ConcreteEdgesGraph.java

--- src/P1/graph/ConcreteEdgesGraph.java
+++ src/P1/graph/ConcreteEdgesGraph.java
@@ -23,7 +23,7 @@ public class ConcreteEdgesGraph<L> implements Graph<L> {
 
     // Abstraction function:
     // TODO
-    /**
+    /*
      * use a universe L to represent vertices; use edge class to represent edges
      */
 
@@ -46,88 +46,94 @@ public class ConcreteEdgesGraph<L> implements Graph<L> {
 
     // TODO checkRep
     private void checkRep() {
-        for (L vertex : vertices)
-            assert (vertex != null);
-        for (Edge edge : edges)
-            assert (edge != null);
+	for (L vertex : vertices)
+	    assert (vertex != null);
+	for (Edge<L> edge : edges)
+	    assert (edge != null);
     }
 
     @Override
     public boolean add(L vertex) {
-        if (vertices.contains(vertex))
-            return false;
-        vertices.add(vertex);
-        return true;
+	if (vertices.contains(vertex))
+	    return false;
+	vertices.add(vertex);
+	return true;
     }
 
     @Override
     public int set(L source, L target, int weight) {
-        if (weight < 0)
-            throw new RuntimeException("Negative weight");
-        // Find the same edge
-        for (Edge edge : edges) {
-            if (edge.sameEdge(source, target)) {
-                int lastEdgeWeight = edge.weight();
-                edges.remove(edge);
-                Edge instance = new Edge(source, target, weight);
-                edges.add(instance);
-                checkRep();
-                return lastEdgeWeight;
-            }
-        }
-        // weight=0 means delete an edge, so it can't be before FINDING
-        if (weight == 0)
-            return 0;
-        // new positive edge
-        Edge instance = new Edge(source, target, weight);
-        edges.add(instance);
-        checkRep();
-        return 0;
+	if (weight < 0)
+	    throw new RuntimeException("Negative weight");
+	if (!vertices.contains(source) || !vertices.contains(target))
+	    throw new NullPointerException("Inexistent vertex");
+	if (source.equals(target)) // source is the same with target, REFUSE to set the Edge.
+	    return 0;
+	// Find the same edge
+	for (Edge<L> edge : edges) {
+	    if (edge.sameEdge(source, target)) {
+		int lastEdgeWeight = edge.weight();
+		edges.remove(edge);
+		if (weight > 0) {
+		    Edge<L> newEdge = new Edge(source, target, weight);
+		    edges.add(newEdge);
+		}
+		checkRep();
+		return lastEdgeWeight;
+	    }
+	}
+	// weight=0 means delete an edge, so it can't be before FINDING
+	if (weight == 0)
+	    return 0;
+	// new positive edge
+	Edge<L> newEdge = new Edge(source, target, weight);
+	edges.add(newEdge);
+	checkRep();
+	return 0;
     }
 
     @Override
     public boolean remove(L vertex) {
-        if (!vertices.contains(vertex))
-            return false;
-        edges.removeIf(edge -> edge.source().equals(vertex) || edge.target().equals(vertex));
-        vertices.remove(vertex);
-        checkRep();
-        return true;
+	if (!vertices.contains(vertex))
+	    return false;
+	edges.removeIf(edge -> edge.source().equals(vertex) || edge.target().equals(vertex));
+	vertices.remove(vertex);
+	checkRep();
+	return true;
     }
 
     @Override
     public Set<L> vertices() {
-        return new HashSet<>(vertices);
+	return new HashSet<>(vertices);
     }
 
     @Override
     public Map<L, Integer> sources(L target) {
-        Map<L, Integer> sources = new HashMap<>();
-        for (Edge edge : edges) {
-            if (target.equals(edge.target())) {
-                sources.put((L) edge.source(), edge.weight());
-            }
-        }
-        checkRep();
-        return sources;
+	Map<L, Integer> sources = new HashMap<>();
+	for (Edge<L> edge : edges) {
+	    if (target.equals(edge.target())) {
+		sources.put((L) edge.source(), edge.weight());
+	    }
+	}
+	checkRep();
+	return sources;
     }
 
     @Override
     public Map<L, Integer> targets(L source) {
-        Map<L, Integer> targets = new HashMap<>();
-        for (Edge edge : edges) {
-            if (source.equals(edge.source())) {
-                targets.put((L) edge.target(), edge.weight());
-            }
-        }
-        checkRep();
-        return targets;
+	Map<L, Integer> targets = new HashMap<>();
+	for (Edge<L> edge : edges) {
+	    if (source.equals(edge.source())) {
+		targets.put((L) edge.target(), edge.weight());
+	    }
+	}
+	checkRep();
+	return targets;
     }
 
     // TODO toL()
     @Override
     public String toString() {
-        return "the number of vertices is  " + vertices.size() + " ,the number of edges is " + edges.size();
+	return "the number of vertices is  " + vertices.size() + " ,the number of edges is " + edges.size();
     }
 
 }
@@ -149,7 +155,8 @@ class Edge<L> {
     // Abstraction function:
     // TODO
     /*
-     * source: the start vertex of the edge target: the end vertex of the edge
+     * source: the start vertex of the edge
+     * target: the end vertex of the edge
      * weight: the value of the edge
      */
 
@@ -167,37 +174,37 @@ class Edge<L> {
 
     // TODO constructor
     Edge(L source, L target, int weight) {
-        this.source = source;
-        this.target = target;
-        this.weight = weight;
-        checkRep();
+	this.source = source;
+	this.target = target;
+	this.weight = weight;
+	checkRep();
     }
 
     // TODO checkRep
     private void checkRep() {
-        assert (weight > 0 || source == target);
+	assert (weight > 0 || source == target);
     }
 
     // TODO methods
     public L source() {
-        return this.source;
+	return this.source;
     }
 
     public L target() {
-        return this.target;
+	return this.target;
     }
 
     public int weight() {
-        return this.weight;
+	return this.weight;
     }
 
     public boolean sameEdge(L source, L target) {
-        return source == this.source && target == this.target;
+	return source == this.source && target == this.target;
     }
 
     // TODO toL()
     @Override
     public String toString() {
-        return "Source: " + this.source + "; Target: " + this.target + "; Weight: " + this.weight;
+	return "Source: " + this.source + "; Target: " + this.target + "; Weight: " + this.weight;
     }
 }
