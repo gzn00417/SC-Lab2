403fa199850648c84737fb5883eb6634fdd9585e
Guo Zhuoning <847738428@qq.com>
2020-03-13 12:01:24 +0800

    Reused

--- src/P2/FriendshipGraph.java
+++ src/P2/FriendshipGraph.java
@@ -3,81 +3,71 @@ package P2;
 import P1.graph.*;
 
 import java.util.*;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class FriendshipGraph {
-    private Graph<Person> graph = new Graph();
+public abstract class FriendshipGraph {
+    public abstract Graph<Person> emptyInstance();
+
+    Graph<Person> graph = emptyInstance();
 
     /**
-     * @method addVertex add new vertex if the person's name is unduplicated
      * @param newPerson adding person
-     * @param head      head of Linked List of persons
-     * @param NameSet   set of persons' names, used for removing duplication
+     * @return weather add successfully
      */
 
-    private Node head = null;
-    private HashSet<String> NameSet = new HashSet<>();
-
-    public void addVertex(Person newPerson) {
-	if (NameSet.contains(newPerson.Name)) {
-	    System.out.println("Person " + newPerson.Name + " already existed.");
-	    System.exit(0);
-	}
-	NameSet.add(newPerson.Name);
-	Node NewVertex = new Node();
-	newPerson.node = NewVertex;
-	NewVertex.LoadData(newPerson);
-	if (head == null)
-	    head = NewVertex;
-	else
-	    head.addNode(NewVertex);
-	return;
+    public boolean addVertex(Person newPerson) {
+	return graph.add(newPerson);
     }
 
     /**
-     * @method addEdge add edges of double directions
+     * addEdge add edges of double directions
+     * 
      * @param a,b 2 persons being linking with an edge
      * @param A,B 2 nodes of the 2 persons
      */
 
-    public void addEdge(Person a, Person b) {
-	if (a == b) {
-	    System.out.println("They are the same one.");
-	    System.exit(0);
-	}
-	Node A = a.node, B = b.node;
-	A.addNodeEdge(B);
-	B.addNodeEdge(A);
-	return;
+    public int addEdge(Person a, Person b) {
+	int lastEdgeWeight;
+	lastEdgeWeight = graph.set(a, b, 1);
+	lastEdgeWeight = graph.set(b, a, 1);
+	return lastEdgeWeight;
     }
 
     /**
-     * @method getDistance
+     * get Distance of two of them
+     * 
      * @param sta path starting person
      * @param end path ending person
      * @return distance between 2 persons or -1 when unlinked
      */
 
+    Map<Person, Integer> dis = new HashMap<>();
+    Map<Person, Boolean> vis = new HashMap<>();
+
     public int getDistance(Person sta, Person end) {
 	if (sta == end)
 	    return 0;
 	Queue<Person> qu = new LinkedList<Person>();
-	for (Node p = head; p != null; p = p.next) {
-	    p.vis = false;
-	    p.dis = 0;
+	Set<Person> persons = graph.vertices();
+	for (Person person : persons) {
+	    dis.put(person, 0);
+	    vis.put(person, false);
 	}
-	sta.node.vis = true;
+	vis.remove(sta);
+	vis.put(sta, true);
 	for (qu.offer(sta); !qu.isEmpty();) {
-	    Person p = qu.poll();
-	    for (Node.Edge e = p.node.lastEdge; e != null; e = e.nextEdge) {
-		if (!e.terminal.vis) {
-		    qu.offer(e.terminal.person);
-		    e.terminal.vis = true;
-		    e.terminal.dis = p.node.dis + 1;
-		    if (e.terminal.person == end)
-			return end.node.dis;
+	    Person person = qu.poll();
+	    for (Map.Entry<Person, Integer> edge : graph.targets(person).entrySet()) {
+		Person target = edge.getKey();
+		if (!vis.get(target)) {
+		    qu.offer(target);
+		    vis.remove(target);
+		    vis.put(target, true);
+		    dis.remove(target);
+		    dis.put(target, dis.get(person) + 1);
+		    if (target == end)
+			return dis.get(target);
 		}
 	    }
 	}
--- src/P2/Person.java
+++ src/P2/Person.java
@@ -2,9 +2,8 @@ package P2;
 
 public class Person {
     public String Name;
-    public FriendshipGraph.Node node = null;
 
     public Person(String PersonName) {
-        Name = PersonName;
+	Name = PersonName;
     }
 }
