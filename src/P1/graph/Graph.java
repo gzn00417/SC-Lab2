3909251f2dcec8143ce87e4f46df711252b2b81a
Guo Zhuoning <847738428@qq.com>
2020-03-13 11:34:36 +0800

    Update Graph.java

--- src/P1/graph/Graph.java
+++ src/P1/graph/Graph.java
@@ -26,7 +26,7 @@ public interface Graph<L> {
      * @return a new empty weighted directed graph
      */
     public static <L> Graph<L> empty() {
-        return new ConcreteEdgesGraph();
+        return new ConcreteEdgesGraph<L>();
     }
     
     /**
