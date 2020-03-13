c327c533c02551c106e8c6e38df4b1cfeea21bc7
Guo Zhuoning <847738428@qq.com>
2020-03-13 11:27:37 +0800

    Pass Test

--- test/P1/poet/GraphPoetTest.java
+++ test/P1/poet/GraphPoetTest.java
@@ -5,6 +5,9 @@ package P1.poet;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.Test;
 
 /**
@@ -13,13 +16,30 @@ import org.junit.Test;
 public class GraphPoetTest {
     
     // Testing strategy
-    //   TODO
+    //
+    // Bridge Word Path:
+    // no two-edge-long path
+    // one two-edge-long path
+    // more than one two-edge-long path (the same / different)
+    //
+    // Words:
+    // 0 vertex exciting
+    // 1 vertex exciting
+    // 2 vertex exciting
     
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
     
-    // TODO tests
-    
+    // tests
+    @Test
+    public void test() throws IOException {
+	final GraphPoet nimoy = new GraphPoet(new File("test/P1/poet/sentence.txt"));
+        final String input = "Seek to explore new and exciting synergies!";
+        String output = nimoy.poem(input);
+        System.out.println(input + "\n>>>\n" + output);
+        assertEquals("Seek to explore strange new life and exciting synergies!", output);
+    }
+
 }
