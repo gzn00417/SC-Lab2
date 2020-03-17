3a95cdbd17ac0c1de09b38ee237fe8f672dc764a
Guo Zhuoning <847738428@qq.com>
2020-03-17 19:01:32 +0800

    add other L tests

--- test/P1/graph/GraphStaticTest.java
+++ test/P1/graph/GraphStaticTest.java
@@ -5,6 +5,9 @@ package P1.graph;
 
 import static org.junit.Assert.*;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 /**
@@ -31,5 +34,21 @@ public class GraphStaticTest {
     }
 
     // TODO test other vertex label types in Problem 3.2
+    @Test
+    public void testOtherType() {
+        Graph<Integer> emptyInstance = Graph.empty();
+        assertTrue(emptyInstance.add(1));
+        assertTrue(emptyInstance.add(2));
+        assertTrue(emptyInstance.add(3));
+        assertEquals(0, emptyInstance.set(2, 1, 4));
+        assertEquals(0, emptyInstance.set(2, 3, 5));
+
+        Map<Integer, Integer> targets = new HashMap<>();
+        targets.put(1, 4);
+        targets.put(3, 5);
+        assertEquals(targets, emptyInstance.targets(2));
+        /* this case is to test if the source vertex has no out coming Edges */
+        assertTrue("expect empty map", emptyInstance.targets(3).isEmpty());
+    }
 
 }
