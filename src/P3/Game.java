2eadf9e3d6810601ad7b427ee57552e57afbb2c4
Guo Zhuoning <gzn00417@foxmail.com>
2020-03-21 16:16:35 +0800

    Update Game.java

--- src/P3/Game.java
+++ src/P3/Game.java
@@ -7,7 +7,7 @@ public interface Game {
 
     /**
      * to generate a particular game according to the String gameType
-     * @param gameType
+     * @param gameType String of the type of the game
      * @return a new chessGame or goGame
      */
     public static Game newGame(String gameType) {
@@ -22,9 +22,9 @@ public interface Game {
      * the piece should belong to the player and be out of the board
      * the position should be free and legally in the board
      *
-     * @param player
-     * @param piece
-     * @param position
+     * @param player the operating player
+     * @param piece the putting piece
+     * @param position the position related to the action
      * @return true if the put is legal
      */
     public boolean put(Player player, Piece piece, Position position);
@@ -37,8 +37,8 @@ public interface Game {
      * the player should be gaming
      * the source-piece should belong to the player and the target-piece should belong to another player
      * 
-     * @param player
-     * @param positions
+     * @param player the operating player
+     * @param positions the position related to the action
      * @return true if the move is legal
      */
     public boolean move(Player player, Position... positions);
@@ -51,8 +51,8 @@ public interface Game {
      * the source-piece should belong to the player and the target-piece should belong to another player
      * if the game is go, there is no target position
      * 
-     * @param player
-     * @param positions
+     * @param player the operating player
+     * @param positions the position related to the action
      * @return true if the capture is legal
      */
     public boolean capture(Player player, Position... positions);
@@ -63,7 +63,7 @@ public interface Game {
      * 
      * the position should be within the board
      * 
-     * @param position
+     * @param position the position related to the action
      * @return player if the position is occupied, null if the position is free
      */
     public Player isFree(int x, int y);
@@ -89,14 +89,14 @@ public interface Game {
 
     /**
      * the origin pieces of one particular game
-     * @param firstFlag
+     * @param firstFlag true if the pieces belong to the first hand player, false if not
      * @return the set of pieces initialized
      */
     public Set<Piece> pieces(boolean firstFlag);
 
     /**
-     * @param p1
-     * @param p2
+     * @param p1 the first hand player
+     * @param p2 the later hand player
      * @return true if successfully set the first and second players
      */
     public boolean setPlayers(Player p1, Player p2);
@@ -122,7 +122,7 @@ public interface Game {
     public Board board();
 
     /**
-     * @param playerName
+     * @param playerName String of the name of a player
      * @return the player who owns the name
      */
     public Player choosePlayerByName(String playerName);
